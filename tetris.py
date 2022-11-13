0613264ce92d1e2fa17214d70e90807c73cbfd02
Dio Fadillah <90459319+dio-fadillah@users.noreply.github.com>
2022-11-13 14:37:41 +0700

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -1,1 +1,10 @@
-tetris
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+st.caption("Author : Dio Fadillah | Linkedin : https://www.linkedin.com/in/dio-fadillah-4a6157116/")
+
+st.image('Capstone_DIO FADILLAH.png')
